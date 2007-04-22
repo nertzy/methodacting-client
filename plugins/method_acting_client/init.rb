68eaefd11e6b5c94d8b611f7d916642a4169c117
gregory.g.foster <gregory.g.foster@1aeb1bae-b52e-0410-8eac-57161a6122d5>
2007-04-22 16:41:40 +0000

    Requires.

--- plugins/method_acting_client/init.rb
+++ plugins/method_acting_client/init.rb
@@ -1,1 +1,7 @@
-# Include hook code here
+require 'active_resource'
+
+# Load plugin models
+require 'method_acting_client/lib/method_acting_resource'
+require 'method_acting_client/lib/user'
+require 'method_acting_client/lib/actor'
+require 'method_acting_client/lib/connection'
