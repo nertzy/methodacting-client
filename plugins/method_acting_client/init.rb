c880a964c1c0425d8a877ac3313a0760383df0a4
nertzy <nertzy@1aeb1bae-b52e-0410-8eac-57161a6122d5>
2007-04-22 17:10:39 +0000

    Use /vendor/plugins to load path of plugin models requires

--- plugins/method_acting_client/init.rb
+++ plugins/method_acting_client/init.rb
@@ -2,7 +2,7 @@ require 'vendor/rails/activesupport/lib/active_support'
 require 'vendor/rails/activeresource/lib/active_resource'
 
 # Load plugin models
-require 'method_acting_client/lib/method_acting_resource'
-require 'method_acting_client/lib/user'
-require 'method_acting_client/lib/actor'
-require 'method_acting_client/lib/connection'
+require 'vendor/plugins/method_acting_client/lib/method_acting_resource'
+require 'vendor/plugins/method_acting_client/lib/user'
+require 'vendor/plugins/method_acting_client/lib/actor'
+require 'vendor/plugins/method_acting_client/lib/connection'
